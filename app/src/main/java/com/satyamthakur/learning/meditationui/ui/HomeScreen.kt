4ee8d694c8619ba7c2df22072ca2992596370078
Satyam Thakur <sat.thakur20@gmail.com>
2023-09-28 18:54:30 +0530

    Working on HomeScreen and adding fonts in FontFamily in Type.kt file

---
+++ app/src/main/java/com/satyamthakur/learning/meditationui/ui/HomeScreen.kt
@@ -0,0 +1,54 @@
+package com.satyamthakur.learning.meditationui.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.satyamthakur.learning.meditationui.ui.theme.DeepBlue
+
+@Composable
+fun HomeScreen() {
+    Box(modifier = Modifier
+        .background(DeepBlue)
+        .fillMaxSize()
+    ) {
+        Column {
+
+        }
+    }
+}
+
+@Composable
+fun GreetingSection(
+    name: String = "Neo"
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Good Morning, $name",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "We wish you have a good day!",
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
+}
