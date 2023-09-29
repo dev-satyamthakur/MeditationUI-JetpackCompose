c0b1f54e508e0add98746432c0da752dd286ea4b
Satyam Thakur <sat.thakur20@gmail.com>
2023-09-29 16:38:07 +0530

    Creating an options chip section composable

--- app/src/main/java/com/satyamthakur/learning/meditationui/ui/HomeScreen.kt
+++ app/src/main/java/com/satyamthakur/learning/meditationui/ui/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.satyamthakur.learning.meditationui.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,17 +9,27 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.satyamthakur.learning.meditationui.R
+import com.satyamthakur.learning.meditationui.ui.theme.ButtonBlue
+import com.satyamthakur.learning.meditationui.ui.theme.DarkerButtonBlue
 import com.satyamthakur.learning.meditationui.ui.theme.DeepBlue
+import com.satyamthakur.learning.meditationui.ui.theme.TextWhite
 
 @Composable
 fun HomeScreen() {
@@ -29,6 +40,7 @@ fun HomeScreen() {
     ) {
         Column {
             GreetingSection()
+            ChipOptionSection(chips = listOf("Sweet Sleep", "Insomnia", "Anxiety"))
         }
     }
 }
@@ -63,5 +75,31 @@ fun GreetingSection(
             tint = Color.White,
             modifier = Modifier.padding(24.dp)
         )
+    }
+}
+
+@Composable
+fun ChipOptionSection(
+    chips: List<String>
+) {
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+    LazyRow {
+        items(chips.size) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                .clickable {
+                    selectedChipIndex = it
+                }
+                .clip(RoundedCornerShape(10.dp))
+                .background(if (selectedChipIndex == it) ButtonBlue else DarkerButtonBlue)
+                .padding(15.dp)
+            ) {
+                Text(text = chips[it], color = TextWhite)
+            }
+        }
     }
 }
